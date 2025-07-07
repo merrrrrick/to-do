a67b6abcbd1acb73c1d860f60d3f07c90206106f
merrrrrick <merrickpereira@gmail.com>
2025-07-08 02:33:19 +0530

    r

--- backend/src/middleware/authUser.js
+++ backend/src/middleware/authUser.js
@@ -1,10 +1,10 @@
 import jwt from 'jsonwebtoken';
 
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET = 'your_secret_key';
 
 export function getUserIdFromToken(req, res, next) {
     const authHeader = req.headers.authorization;
-
+console.log(authHeader)
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
         return res.status(401).json({ error: 'Authorization token missing or invalid' });
     }

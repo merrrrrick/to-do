549cc7631286ee1f202ea1e9da0803563935aa22
merrrrrick <merrickpereira@gmail.com>
2025-07-08 02:50:18 +0530

    haa

--- frontend/my-app/next.config.mjs
+++ frontend/my-app/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
+    typescript: {
     // ✅ Ignore TypeScript build errors
     ignoreBuildErrors: true,
   },
@@ -10,4 +10,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
